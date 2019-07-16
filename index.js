3dd8fbffa3182b84734d8a2511781e46c308eadc
Alec Hall <alecnhall@gmail.com>
2019-07-16 21:35:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,7 +10,9 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
 }
 
 function deleteFromObjectByKey(object, key){
-
+  var newObj = Object.assign({}, object);
+  delete newObj.key;
+  return object;
 }
 
 function destructivelyDeleteFromObjectByKey(object, key){
