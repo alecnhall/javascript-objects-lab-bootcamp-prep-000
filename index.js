0ae28f3328ded946fd5c16d7be37b332b6146289
Alec Hall <alecnhall@gmail.com>
2019-07-16 20:15:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var recipes = {key: 'value'}
 
 function updateObjectWithKeyAndValue(object, key, value){
   var newObject = Object.assign({}, object);
-  return newObject;
+  return object;
 }
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
   object[key] = value;
